3041732e3d24e43c43dcfd9b20deb3f6a467bcac
James Leahy <defuncart@gmail.com>
2021-01-17 09:06:35 +0100

    Add KifuViewer.

---
+++ lib/widgets/home_screen/kifu_viewer.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shogi_board/flutter_shogi_board.dart';
+import 'package:shogi/shogi.dart';
+
+class KifuViewer extends StatefulWidget {
+  KifuViewer({
+    Key key,
+    @required this.game,
+  }) : super(key: key);
+
+  final List<GameBoard> game;
+
+  @override
+  _KifuViewerState createState() => _KifuViewerState();
+}
+
+class _KifuViewerState extends State<KifuViewer> {
+  int _currentIndex = 0;
+
+  GameBoard get gameBoard => widget.game[_currentIndex];
+  bool get _canSkipStart => _currentIndex != 0;
+  bool get _canRewind => _currentIndex > 0;
+  bool get _canFastForward => _currentIndex < widget.game.length - 1;
+  bool get _canSkipEnd => _currentIndex != widget.game.length - 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: ShogiBoard(
+                gameBoard: gameBoard,
+                showPiecesInHand: false,
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.skip_previous),
+                  onPressed: _canSkipStart ? () => setState(() => _currentIndex = 0) : null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.fast_rewind),
+                  onPressed: _canRewind ? () => setState(() => _currentIndex--) : null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.fast_forward),
+                  onPressed: _canFastForward ? () => setState(() => _currentIndex++) : null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.skip_next),
+                  onPressed: _canSkipEnd ? () => setState(() => _currentIndex = widget.game.length - 1) : null,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
