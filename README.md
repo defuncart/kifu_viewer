2f5f9fa442ace362bb0ba7723ed2c2bc1f482425
James Leahy <defuncart@gmail.com>
2021-01-30 11:30:15 +0100

    Feature/step through moves using arrow shortcuts (#7)
    Adds ability to step through moves using arrow shortcuts. Removes all flutter defaults, except for ESC to close popups.

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 
 Shogi (将棋) is a two-player strategy board game native to Japan, belonging to the same family as chess and xiangqi. Kifu (棋譜) is the Japanese term for abstract strategy game record.
 
-Kifu Viewer is a desktop application which allows the loading of `kif` files and viewing shogi games.
+Kifu Viewer is a desktop application which allows the loading of `kif` files and viewing shogi games. **This application does not feature a shogi engine, thus the user cannot analyze games nor play against the computer.**
 
-*NOTE* that this application does not feature a shogi engine, thus the user cannot analyze games nor play against the computer.
+## How to Use
+
+A `kif` file can be loaded by either opening a file or pasting from system clipboard. Both these actions can be achieved via the menu bar, shortcuts or via buttons in the upper-right corner of the application.
+
+Once a kif file is loaded, moves can be stepped through using the arrow keys:
+
+| Key   | Action               |
+|-------|----------------------|
+| UP    | To the initial board |
+| LEFT  | Back one move        |
+| RIGHT | Forward one move     |
+| DOWN  | To the final board   |
 
 ![](docs/screenshots/01.gif)
 
@@ -31,6 +42,8 @@ If you would like to build the app yourself, then you simply need:
     - libgtk-3-dev
     - libblkid-dev
 
-## Contributing
+## Raising Issues and Contributing
 
-The project isn't actively looking for contributors, however feel free to contact James here on GitHub or [Twitter](https://twitter.com/defuncart).
+Please report bugs and issues, and raise feature requests [here](https://github.com/defuncart/kifu_viewer/issues).
+
+To contribute, submit a PR with a detailed description and tests, if applicable.
--- lib/widgets/home_screen/kifu_viewer.dart
+++ lib/widgets/home_screen/kifu_viewer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show LogicalKeyboardKey;
 import 'package:flutter_shogi_board/flutter_shogi_board.dart';
 import 'package:shogi/shogi.dart';
 
@@ -34,70 +35,124 @@ class _KifuViewerState extends State<KifuViewer> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          Container(
-            width: 120,
-            child: ListView.builder(
-              itemCount: widget.game.moves.length,
-              itemBuilder: (_, index) {
-                final isSelected = index == _currentIndex - 1;
-                return Container(
-                  color: isSelected ? Theme.of(context).accentColor : Colors.transparent,
-                  child: GestureDetector(
-                    child: Text(
-                      widget.game.moves[index].asKif,
-                      style: Theme.of(context).textTheme.bodyText2.apply(
-                            color: isSelected ? Colors.white : Theme.of(context).textTheme.bodyText2.color,
-                          ),
+    return FocusableActionDetector(
+      shortcuts: {
+        if (_canSkipStart) LogicalKeySet(LogicalKeyboardKey.arrowUp): const _NavigateIntent.start(),
+        if (_canRewind) LogicalKeySet(LogicalKeyboardKey.arrowLeft): const _NavigateIntent.previous(),
+        if (_canFastForward) LogicalKeySet(LogicalKeyboardKey.arrowRight): const _NavigateIntent.next(),
+        if (_canSkipEnd) LogicalKeySet(LogicalKeyboardKey.arrowDown): const _NavigateIntent.end(),
+      },
+      actions: {
+        _NavigateIntent: CallbackAction<_NavigateIntent>(
+          onInvoke: (intent) => _navigate(intent.navigateType),
+        ),
+      },
+      autofocus: true,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Container(
+              width: 120,
+              child: ListView.builder(
+                itemCount: widget.game.moves.length,
+                itemBuilder: (_, index) {
+                  final isSelected = index == _currentIndex - 1;
+                  return Container(
+                    color: isSelected ? Theme.of(context).accentColor : Colors.transparent,
+                    child: GestureDetector(
+                      child: Text(
+                        widget.game.moves[index].asKif,
+                        style: Theme.of(context).textTheme.bodyText2.apply(
+                              color: isSelected ? Colors.white : Theme.of(context).textTheme.bodyText2.color,
+                            ),
+                      ),
+                      onTap: () => setState(() => _currentIndex = index + 1),
                     ),
-                    onTap: () => setState(() => _currentIndex = index + 1),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-          VerticalDivider(),
-          SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ShogiBoard(
-                    gameBoard: gameBoard,
+            VerticalDivider(),
+            SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: ShogiBoard(
+                      gameBoard: gameBoard,
+                    ),
                   ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.skip_previous),
-                      onPressed: _canSkipStart ? () => setState(() => _currentIndex = 0) : null,
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.fast_rewind),
-                      onPressed: _canRewind ? () => setState(() => _currentIndex--) : null,
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.fast_forward),
-                      onPressed: _canFastForward ? () => setState(() => _currentIndex++) : null,
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.skip_next),
-                      onPressed:
-                          _canSkipEnd ? () => setState(() => _currentIndex = widget.game.gameBoards.length - 1) : null,
-                    ),
-                  ],
-                )
-              ],
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.skip_previous),
+                        onPressed: _canSkipStart ? () => _navigate(_NavigateType.start) : null,
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.fast_rewind),
+                        onPressed: _canRewind ? () => _navigate(_NavigateType.previous) : null,
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.fast_forward),
+                        onPressed: _canFastForward ? () => _navigate(_NavigateType.next) : null,
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.skip_next),
+                        onPressed: _canSkipEnd ? () => _navigate(_NavigateType.end) : null,
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+
+  void _navigate(_NavigateType navigateType) {
+    switch (navigateType) {
+      case _NavigateType.start:
+        if (_canSkipStart) {
+          setState(() => _currentIndex = 0);
+        }
+        break;
+      case _NavigateType.previous:
+        if (_canRewind) {
+          setState(() => _currentIndex--);
+        }
+        break;
+      case _NavigateType.next:
+        if (_canFastForward) {
+          setState(() => _currentIndex++);
+        }
+        break;
+      case _NavigateType.end:
+        if (_canSkipEnd) {
+          setState(() => _currentIndex = widget.game.gameBoards.length - 1);
+        }
+        break;
+    }
+  }
+}
+
+enum _NavigateType {
+  start,
+  previous,
+  next,
+  end,
+}
+
+class _NavigateIntent extends Intent {
+  const _NavigateIntent.start() : navigateType = _NavigateType.start;
+  const _NavigateIntent.previous() : navigateType = _NavigateType.previous;
+  const _NavigateIntent.next() : navigateType = _NavigateType.next;
+  const _NavigateIntent.end() : navigateType = _NavigateType.end;
+
+  final _NavigateType navigateType;
 }
